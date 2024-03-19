648bb3eeee3ac003cdd09ae3a2225f02bd246a56
Valeria <vale_0722@live.com>
2024-03-19 16:09:34 -0500

    feat: Lab1 first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LabsCM20241-Gr01"
+include(":Lab1-UI")
